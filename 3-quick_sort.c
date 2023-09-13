390893940c1a21fe3b42f72c6a20e422ced90342
JOTALGS <jose,pgilsuarez@gmail.com>
2023-09-13 13:39:21 -0700

    BETTY

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,60 +1,61 @@
 #include "sort.h"
 
-void swap(int *a, int *b)
+void
+swap(int *a, int *b)
 {
-int tmp;
+	int tmp;
 
-tmp = *a;
-*a = *b;
-*b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 size_t
 partition(int *arr, size_t low, size_t high, size_t size)
 {
-int pivot = arr[high];
-size_t i = (low - 1);
-size_t j;
+	int pivot = arr[high];
+	size_t i = (low - 1);
+	size_t j;
 
-for (j = low; j <= high; j++)
-{
-	if (arr[j] < pivot)
+	for (j = low; j <= high; j++)
 	{
-		i++;
-		if (i != j)
+		if (arr[j] < pivot)
 		{
-			swap(&arr[i],&arr[j]);
-			print_array(arr, size);
-		}   
+			i++;
+			if (i != j)
+			{
+				swap(&arr[i],&arr[j]);
+				print_array(arr, size);
+			}   
+		}
 	}
-}
-if (arr[i + 1] != arr[high])
-{
-	swap(&arr[i + 1],&arr[high]);
-	print_array(arr, size);
-}
-return (i + 1); 
+	if (arr[i + 1] != arr[high])
+	{
+		swap(&arr[i + 1],&arr[high]);
+		print_array(arr, size);
+	}
+	return (i + 1); 
 }
 
 void
 qs_recursive(int *array, int first, int last, size_t size)
 {
-int piv;
+	int piv;
 
-if (first < last)
-{
-	piv = partition(array, first, last, size);
+	if (first < last)
+	{
+		piv = partition(array, first, last, size);
 
-	qs_recursive(array, first, piv - 1, size);
-	qs_recursive(array, piv + 1, last, size);
-}
+		qs_recursive(array, first, piv - 1, size);
+		qs_recursive(array, piv + 1, last, size);
+	}
 }
 
 void
 quick_sort(int *array, size_t size)
 {
-size_t f = 0;
-size_t l = size - 1;
+	size_t f = 0;
+	size_t l = size - 1;
 
-qs_recursive(array, f, l, size);
+	qs_recursive(array, f, l, size);
 }
