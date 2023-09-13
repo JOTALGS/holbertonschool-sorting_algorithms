87a3d0cca4c4503e1b1598c9d1c4f8c29d8d1bb4
JOTALGS <jose,pgilsuarez@gmail.com>
2023-09-13 13:29:36 -0700

    bwtty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,32 +1,34 @@
 #include "sort.h"
 
-void swap(int *a, int *b)
+void
+swap(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = a;
-    *a = *b;
-    b = tmp;
+	tmp = a;
+	*a = *b;
+	b = tmp;
 }
 
-void bubble_sort(int *array, size_t size)
+void
+bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int ssw;
+	size_t i, j;
+	int ssw;
 
-    for (j = 0; j < size; j++)
-    {
-        ssw = 0;
-        for (i = 0; i < size - j - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                swap(&array[i], &array[i + 1]);
-                ssw = 1;
-                print_array(array, size);
-            }
-        }
-        if (ssw == 0)
-            break;
-    }
+	for (j = 0; j < size; j++)
+	{
+		ssw = 0;
+		for (i = 0; i < size - j - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(&array[i], &array[i + 1]);
+				ssw = 1;
+				print_array(array, size);
+			}
+		}
+		if (ssw == 0)
+			break;
+	}
 }
